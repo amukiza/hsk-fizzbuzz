fa025ef3125e8adfdd993ba3260a5e28df877e92
Andrew Mukiza <andrewmukiza@gmail.com>
2014-05-05 09:15:08 +0300

    added numbers upto 10 and refatcored dto use guards

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -1,4 +1,8 @@
 module FizzBuzz where
 
-fizzBuzz :: Int -> Int
-fizzBuzz num  = num
+fizzBuzz :: Int -> (String, Int)
+fizzBuzz num
+      | num == 5 || mod num 5 == 0 = ("Buzz", 0)
+      | num == 3 || mod num 3 == 0 = ("Fizz", 0)
+      | mod num  3 == 0 && mod num  5 == 0 =  ("Fizz",  0)
+      |otherwise                         = ("", num)
--- FizzBuzzSpec.hs
+++ FizzBuzzSpec.hs
@@ -4,13 +4,28 @@ import Test.Hspec
 import FizzBuzz
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
     describe "FizzBuzz" $ do
     it "returns 0 when given 0" $
-        fizzBuzz 0 `shouldBe` 0
+        snd (fizzBuzz 0) `shouldBe` 0
     
     it "returns 1 when given 1" $
-        fizzBuzz 1 `shouldBe` 1
+        snd (fizzBuzz 1) `shouldBe` 1
 
     it "returns 2 when given 2" $
-        fizzBuzz 2 `shouldBe` 2 
+        snd (fizzBuzz 2) `shouldBe` 2 
+
+    it "returns fizz when given 3" $
+        fst (fizzBuzz 3) `shouldBe` "Fizz"
+
+    it "returns 4 when given 4" $
+        snd (fizzBuzz 4) `shouldBe` 4
+
+    it "returns Buzz when given 5" $
+        fst (fizzBuzz 5) `shouldBe` "Buzz"
+
+    it "returns Fizz when given 6" $
+        fst (fizzBuzz 6) `shouldBe` "Fizz"
+
+    it "returns Buzz when given 10" $
+        fst (fizzBuzz 10) `shouldBe` "Buzz"
