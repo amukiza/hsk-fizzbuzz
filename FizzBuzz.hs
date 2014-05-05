066e832e6066033bef044a666c0f2e23b285f382
Andrew Mukiza <andrewmukiza@gmail.com>
2014-05-05 21:36:35 +0300

    refactoring fizzbuzz guards not to check wheher the num is 3 or 5 but use mod

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -3,8 +3,8 @@ module FizzBuzz where
 fizzBuzz :: Int -> (String, Int)
 fizzBuzz num
       | mod num  3 == 0 && mod num  5 == 0 =  ("Fizz Buzz",  0)
-      |5 `elem`  digits num || num == 5 || mod num 5 == 0 = ("Buzz", 0)
-      | 3 `elem` digits num || num == 3 || mod num 3 == 0 = ("Fizz", 0)
+      | 5 `elem` digits num || mod num 5 == 0 = ("Buzz", 0)
+      | 3 `elem` digits num || mod num 3 == 0 = ("Fizz", 0)
       |otherwise                         = ("", num)
 
 
